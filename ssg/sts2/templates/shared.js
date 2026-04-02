dd3f3a016d5a148392740748d2ef4ba83f849787
letstrygg <65213506+letstrygg@users.noreply.github.com>
2026-04-01 17:01:17 -0700

    1

--- ssg/sts2/templates/shared.js
+++ ssg/sts2/templates/shared.js
@@ -213,9 +213,9 @@ export function generateRunCardHtml(run, user) {
     <div class="card-item ${statusClass} ${charClass}" style="display: flex; flex-direction: column;">
         <a href="/users/${user.slug}/runs/${run.id}/" style="text-decoration: none; color: inherit; display: flex; justify-content: space-between; flex-grow: 1;">
             <div class="card-info">
-                <span class="card-name" style="line-height: 1.2;">
+                <span class="card-name" style="line-height: 1.1;">
                     <span style="font-size: 0.7rem; color: var(--gray); text-transform: uppercase; display: block;">${user.display_name}</span>
-                    <span style="font-size: 0.8rem; color: var(--gray); font-weight: normal; display: block; margin-bottom: 3px;">Run ${run.user_run_num}</span>
+                    <span style="font-size: 0.7rem; color: var(--gray); font-weight: normal; display: block; margin-bottom: 2px;">Run ${run.user_run_num}</span>
                     <span style="color: ${charColor}">${charId}</span>
                 </span>
             </div>
