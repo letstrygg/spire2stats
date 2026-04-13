ec1a493c175825fd6759449d33ffbc7fef9162e4
letstrygg <65213506+letstrygg@users.noreply.github.com>
2026-04-13 01:02:53 -0700

    Update info.js

--- ssg/sts2/templates/info.js
+++ ssg/sts2/templates/info.js
@@ -29,17 +29,17 @@ export function infoTemplate() {
                     </mfrac>
                 </math>
                 <br>
-            <ul style="list-style-type: none; padding-left: 0;">
-                <li>C = confidence constant</li>
-                <li>M = global average win rate</li>
-                <li>R = runs with card</li>
-                <li>W = card win rate</li>
-            </ul>
-
-            <h3>Example</h3>
-            <p>Using a Confidence Constant (C) of 5, and a Global Average Win Rate (M) of 20% (0.20).</p>
             
-            <strong>Example 1: The "Lucky" Card (1 run, 1 win)</strong><br>
+                <br>C = confidence constant
+                <br>M = global average win rate
+                <br>R = runs with card
+                <br>W = card win rate
+            
+            <br>
+            <br>
+            <strong>Example 1: The "Lucky" Card (1 run, 1 win)</strong>
+            <br>
+            <br>
             <math xmlns="http://www.w3.org/1998/Math/MathML" display="inline">
                     <mi>Score</mi>
                     <mo>=</mo>
@@ -67,10 +67,11 @@ export function infoTemplate() {
                     <mo>=</mo>
                     <mn>33.3%</mn>
                 </math>
-                
                 <br>
-                
-                <strong>Example 2: The "Proven" Card (10 runs, 6 wins)</strong><br>
+                <br>
+                <strong>Example 2: The "Proven" Card (10 runs, 6 wins)</strong>
+                <br>
+                <br>
                 <math xmlns="http://www.w3.org/1998/Math/MathML" display="inline">
                     <mi>Score</mi>
                     <mo>=</mo>
@@ -98,6 +99,8 @@ export function infoTemplate() {
                     <mo>=</mo>
                     <mn>46.7%</mn>
                 </math>
+                <br>
+                <br>
                 
             <p>Result: The proven 60% card (46.7%) outranks the lucky 100% card (33.3%).</p>
         </div>
